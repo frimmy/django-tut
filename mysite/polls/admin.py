767b02cec3a4b755516afcfc47b9b8c0adc1a197
adrian.frimpong <adrian.frimpong@gmail.com>
2014-04-30 02:10:15 -0500

    git init

---
+++ mysite/polls/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+# Register your models here.
+from polls.models import Poll, Choice
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 4
+
+class PollAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Questions BEYATCH!!!',			{'fields': ['question']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+
+	list_filter = ['pub_date']
+	
+	inlines = [ChoiceInline]
+	search_fields = ['question']
+	# fields = ['pub_date', 'question']
+	# list_display is the tuple of field names to display on the admin panel
+	list_display = ('question', 'pub_date', 'was_published_recently')
+
+admin.site.register(Poll, PollAdmin)
