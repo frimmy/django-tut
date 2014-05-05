d4487066e95e03fe191a302956accf6bd10f26d4
adrian.frimpong <adrian.frimpong@gmail.com>
2014-05-04 20:20:10 -0400

    Tests for was_published_recently method for Polls model now has robust tests in place for future feature-adds

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -13,7 +13,20 @@ class PollMethodTests(TestCase):
 		pub_date is in the future
 		"""
 		future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
-		self.assertEqual(future_poll.was_published_recently(), False, 
-		msg=("YAH DUDE, TEST FAILED, future polls aren't recen't"
-				" if they ain't been made yet..."
-			))
+		self.assertEqual(future_poll.was_published_recently(), False)
+
+	def test_was_published_recently_with_old_poll(self):
+		"""
+		was_published_recently() should return False for polls whose pub_date
+		is older than 1 day
+		"""
+		old_poll = Poll(pub_date=timezone.now() - datetime.timedelta(days=30))
+		self.assertEqual(old_poll.was_published_recently(), False)
+
+	def test_was_published_recently_with_recent_poll(self):
+		"""
+		was_published_recently() should return True for polls whose pub_date
+		is within the last day
+		"""
+		recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
+		self.assertEqual(recent_poll.was_published_recently(), True)
